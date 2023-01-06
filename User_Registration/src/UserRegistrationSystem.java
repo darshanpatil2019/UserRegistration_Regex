10a76ac863b5f67f2ede5e6e5d58c260f341057a
Darshan Patil <iampatildarsh>
2023-01-06 12:57:15 +0530

    Check for users last name validate

--- User_Registration/src/UserRegistrationSystem.java
+++ User_Registration/src/UserRegistrationSystem.java
@@ -5,15 +5,16 @@ import java.util.regex.Pattern;
 public class UserRegistrationSystem {
     //declaring Patterns
     static String firstNameRegex = "^[A-Z]{1}[a-z]{3,}$";
+    static String lastNameRegex = "^[A-Z]{1}[a-z]{3,}$";
 
     //UC1- Method to validate First Name
 
     public static void validateFirstName() {
         System.out.println("Enter First Name :- ");
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
+        String firstName = sc.nextLine();
         Pattern pattern = Pattern.compile(firstNameRegex);
-        Matcher matcher = pattern.matcher(name);
+        Matcher matcher = pattern.matcher(firstName);
         boolean result = matcher.matches();
         if (result) {
             System.out.println("FirstName is Valid");
@@ -22,7 +23,24 @@ public class UserRegistrationSystem {
         }
     }
 
+    //UC2- Method to validate Last Name
+
+    public static void validateLastName() {
+        System.out.println("Enter Last Name :- ");
+        Scanner sc = new Scanner(System.in);
+        String lastName = sc.nextLine();
+        Pattern pattern = Pattern.compile(lastNameRegex);
+        Matcher matcher = pattern.matcher(lastName);
+        boolean result = matcher.matches();
+        if (result) {
+            System.out.println("LastName is Valid");
+        } else {
+            System.out.println("LastName is Invalid");
+        }
+    }
+
     public static void main(String[] args) {
         validateFirstName();
+        validateLastName();
     }
 }
